b399d6c5c753b88fa32556bbec3637b6d98ad92f
Tarun Saraswat <tarunsaraswat77@gmail.com>
2025-01-08 01:14:59 +0530

    Backend Fixed

--- Backend/scripts/classifyTextStyle.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def classify_text_style(text):
-    """Classify text style based on a regex pattern."""
-    if re.match(r'^[A-Z0-9]{2,3}-[A-Z0-9]{1,2}-[0-9]{1,4}$', text):
-        return "bold black"
-    else:
-        return "other"
--- Backend/scripts/cleanText.py
+++
@@ -1,5 +0,0 @@
-import re
-
-def clean_text(text):
-    """Remove unwanted characters and convert text to uppercase."""
-    return re.sub(r'[^A-Za-z0-9]', '', text).upper()
